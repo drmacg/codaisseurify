999249b962a792d9591695b4e8b88334e3bd7528
gbosnjak <bosnjak.gabrijela@gmail.com>
2017-11-12 11:30:37 +0100

    Delete an artist

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'artists#index'
-  resources :artists, only: [:index, :show] do
+  resources :artists, only: [:index, :show, :destroy] do
     resources :songs, only: [:create, :destroy]
   end
 end
