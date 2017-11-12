964feb49ef589af1a0488fe2025ba7ec8259cae7
gbosnjak <bosnjak.gabrijela@gmail.com>
2017-11-12 15:07:16 +0100

    Song and artist validaton

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,8 @@
 class Song < ApplicationRecord
   belongs_to :artist
+
+  validates :track_number, presence: true, numericality: { only_integer: true, greater_than: 0 }
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :duration, presence: true, length: { maximum: 8}
+
 end
