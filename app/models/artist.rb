7e7cef440c90b4490edff59a66b450777bcc2f97
gbosnjak <bosnjak.gabrijela@gmail.com>
2017-11-12 17:13:38 +0100

    Change validations

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,7 +2,7 @@ class Artist < ApplicationRecord
   has_many :songs, dependent: :destroy
 
   validates :name, presence: true, length: { maximum: 50 }
-  validates :biography, presence: true, length: { maximum: 500 }
+  validates :biography, presence: true, length: { maximum: 1000 }
   validates :image_url, presence: true
 
 end
--- spec/models/artist_spec.rb
+++ spec/models/artist_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Artist, type: :model do
     it { is_expected.to validate_presence_of(:name) }
     it { is_expected.to validate_length_of(:name).is_at_most(50) }
     it { is_expected.to validate_presence_of(:biography) }
-    it { is_expected.to validate_length_of(:biography).is_at_most(500) }
+    it { is_expected.to validate_length_of(:biography).is_at_most(1000) }
     it { is_expected.to validate_presence_of(:image_url) }
   end
 
